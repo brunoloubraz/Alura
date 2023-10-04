56bc68fe0b5de2114d6fe27a26d7a8e3a4ad03ab
brunoloubraz <brunolou.braz@gmail.com>
2023-10-03 23:12:56 -0300

    conteudo pela metade

--- java/screnmatch/src/Principal.java
+++ java/screnmatch/src/Principal.java
@@ -1,7 +1,26 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
 public class Principal {
     public static void main(String[] args) {
+    Filme meufilme = new Filme();
+    meufilme.setNome("O poderoso chefão");
+    meufilme.setAnoDeLancamento(1970);
+    meufilme.setDuracaoEmMinutos(180);
 
+    meufilme.exibeFichaTecnica();
+    meufilme.avalia(8);
+    meufilme.avalia(5);
+    meufilme.avalia(10);
+        System.out.println("Total de Avaliacões: " + meufilme.getTotalDeAvaliacoes());
+//        System.out.println(meufilme.somaDasAvaliacoes);
+        System.out.println(meufilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
     }
 }
